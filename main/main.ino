7485fa03e9526acfd5f8df23ce6ac19bef63dac7
StrayRaider <41emrekaya919@gmail.com>
2023-04-13 14:41:26 +0300

    project V0.1.0

--- main/main.ino
+++ main/main.ino
@@ -13,9 +13,9 @@ bool hardness = 0;
 int time = 50;
 int loop_c = 0;
 int cnt = 0;
-const int obj_size = 20;
+const int obj_size = 50;
 
-static int Objects[20][3];
+static int Objects[50][3];
 int b_c_a = 0;
 
 static const bool Rocket[8][8] = {
@@ -100,10 +100,14 @@ int life = 3;
 int gun = 3;
 bool gun_exec = 0;
 int bullet[2] = {-1,-1};
+int tch = 0;
+int prs = 0;
 
 void setup()
 {
   pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  
   pinMode(13, INPUT);
   pinMode(12, INPUT);
   pinMode(11, INPUT);
@@ -128,8 +132,15 @@ void setup()
   pinMode(35, OUTPUT);
   pinMode(36, OUTPUT);
 
-  
+  pinMode(37, OUTPUT);
+  pinMode(38, OUTPUT);
+  pinMode(39, OUTPUT);
+
+  pinMode(40, OUTPUT);
+
   pinMode(41, OUTPUT);
+
+  pinMode(42, OUTPUT);
 
   digitalWrite(23,1);
 
@@ -141,10 +152,22 @@ void setup()
   int down = 0;
   int select = 0;
   int i = 5;
+  bool lD = 0;
   while(1){
-    display.drawRoundRect(2, 2, 120,20,8, SSD1306_WHITE);
-    display.drawRoundRect(2, 32, 120,20,8, SSD1306_WHITE);
     display.display();
+    fill(lD);
+    int ligt = analogRead(A1);
+    if(ligt>500){
+      lD = 1;
+    }else
+      lD = 0;
+    if(!lD){
+      display.drawRoundRect(2, 2, 120,20,8, SSD1306_WHITE);
+      display.drawRoundRect(2, 32, 120,20,8, SSD1306_WHITE);
+    }else{
+      display.drawRoundRect(2, 2, 120,20,8, SSD1306_BLACK);
+      display.drawRoundRect(2, 32, 120,20,8, SSD1306_BLACK);
+    }
     up = digitalRead(12);
     down = digitalRead(13);
     select = digitalRead(11);
@@ -163,27 +186,53 @@ void setup()
       }
     }
     if (act_loc){
-      display.fillRoundRect(4, 4, 116,14,8, SSD1306_WHITE);
-      display.fillRoundRect(4, 34, 116,14,8, SSD1306_BLACK);
+      if(lD){
+        display.fillRoundRect(4, 4, 116,14,8, SSD1306_BLACK);
+        display.fillRoundRect(4, 34, 116,14,8, SSD1306_WHITE);
+      }else{
+        display.fillRoundRect(4, 4, 116,14,8, SSD1306_WHITE);
+        display.fillRoundRect(4, 34, 116,14,8, SSD1306_BLACK);
+      }
       display.setTextSize(1);             // Normal 1:1 pixel scale
-      display.setTextColor(SSD1306_BLACK);        // Draw white text
+      if(lD){
+        display.setTextColor(SSD1306_WHITE);        // Draw white text
+      }else{
+        display.setTextColor(SSD1306_BLACK);        // Draw white text
+      }
       display.setCursor(40,8);           // Start at top-left corner
       if(hardness){
          display.println(F("EASY"));
       }else{
         display.println(F("HARD"));
       }
-      display.setTextColor(SSD1306_WHITE);        // Draw white text
+      if(lD){
+        display.setTextColor(SSD1306_BLACK);        // Draw white text
+      }else{
+        display.setTextColor(SSD1306_WHITE);        // Draw white text
+      }
       display.setCursor(40,38);             // Start at top-left corner
       display.println(F(" Start "));
     }else{
-      display.fillRoundRect(4, 34, 116,14,8, SSD1306_WHITE);
-      display.fillRoundRect(4, 4, 116,14,8, SSD1306_BLACK);
+      if(lD){
+        display.fillRoundRect(4, 34, 116,14,8, SSD1306_BLACK);
+        display.fillRoundRect(4, 4, 116,14,8, SSD1306_WHITE);
+      }else{
+        display.fillRoundRect(4, 34, 116,14,8, SSD1306_WHITE);
+        display.fillRoundRect(4, 4, 116,14,8, SSD1306_BLACK);        
+      }
       display.setTextSize(1);             // Normal 1:1 pixel scale
-      display.setTextColor(SSD1306_BLACK);        // Draw white text
+      if(!lD){
+        display.setTextColor(SSD1306_BLACK);        // Draw white text
+      }else{
+        display.setTextColor(SSD1306_WHITE);        // Draw white text
+      }
       display.setCursor(40,38);             // Start at top-left corner
       display.println(F(" Start "));
-      display.setTextColor(SSD1306_WHITE);        // Draw white text
+      if(lD){
+        display.setTextColor(SSD1306_BLACK);        // Draw white text
+      }else{
+        display.setTextColor(SSD1306_WHITE);        // Draw white text
+      }
       display.setCursor(40,8);             // Start at top-left corner
       if(hardness){
          display.println(F("EASY"));
@@ -191,44 +240,73 @@ void setup()
         display.println(F("HARD"));
       }
     }
-    delay(50);
+    display.display();
+    delay(time);
   }
-  display.display();
-  display.clearDisplay();
   set_map();
 }
 //gets x and y as matris location
-void draw(bool Obj[8][8],int x, int y){
+void draw(bool Obj[8][8],int x, int y,bool t){
   for(int i=0 ; i<8 ; i++){
     for(int j=0 ; j<8 ; j++){
       if(Obj[i][j] == 1)
-        display.drawPixel(x*8+i, y*8+j, SSD1306_WHITE);
+        if(!t)
+          display.drawPixel(x*8+i, y*8+j, SSD1306_WHITE);
+        else
+          display.drawPixel(x*8+i, y*8+j, SSD1306_BLACK);
     }
   }  
 }
 
-void del(bool Obj[8][8],int x, int y){
+void del(bool Obj[8][8],int x, int y,bool t){
   int x_px = x*8;
   int y_px = y*8;
   for(int i=0 ; i<8 ; i++){
     for(int j=0 ; j<8 ; j++){
       if(Obj[i][j] == 1){
-        display.drawPixel(x_px+i, y_px+j, SSD1306_BLACK);
+        if(t)
+          display.drawPixel(x*8+i, y*8+j, SSD1306_WHITE);
+        else
+          display.drawPixel(x*8+i, y*8+j, SSD1306_BLACK);
       }
     }
   }  
 }
 
+void fill(bool t){
+  for(int i=0 ; i<128 ; i++){
+    for(int j=0 ; j<64 ; j++){  
+      if(t)
+        display.drawPixel(i, j, SSD1306_WHITE);
+      else
+        display.drawPixel(i, j, SSD1306_BLACK);
+    }
+  }
+}
 int get_loc(int sensorValue){
   return sensorValue/128;
   //0 to 8 matris data
 }
 
 void gen_met(){
+int randomness = 5;
   if(b_c_a<obj_size){
+    int lr = Objects[b_c_a][1] = random(0,2);
     Objects[b_c_a][0] = 15;
-    Objects[b_c_a][1] = random(1,9);
-    Objects[b_c_a][2] = random(0,4);
+    Objects[b_c_a][1] = lr;
+    Objects[b_c_a][2] = random(0,randomness);
+    b_c_a +=1;
+    Objects[b_c_a][0] = 15;
+    Objects[b_c_a][1] = lr+2;
+    Objects[b_c_a][2] = random(0,randomness);
+    b_c_a +=1;
+    Objects[b_c_a][0] = 15;
+    Objects[b_c_a][1] = lr+4;
+    Objects[b_c_a][2] = random(0,randomness);
+    b_c_a +=1;
+    Objects[b_c_a][0] = 15;
+    Objects[b_c_a][1] = lr+6;
+    Objects[b_c_a][2] = random(0,randomness);
     b_c_a +=1;
   }
   //engel = 0
@@ -259,14 +337,20 @@ void update_bar(int x){
 
 void loop()
 {
+  digitalWrite(41,LOW);
   //rocket events
   int sensorValue = analogRead(A0);
+  int ligt = analogRead(A1);
+  bool lD = 0;
+  if(ligt>500){
+    lD = 1;
+  }else
+    lD = 0;
   //move rocket
-  del(Rocket,0,rocket_loc[1]);
+  del(Rocket,0,rocket_loc[1],lD);
   rocket_loc[1] = get_loc(sensorValue);
 
   //gun exec button
-  digitalWrite(41,LOW);
   if(gun){
     if (digitalRead(10) != gun_exec){
       gun_exec = digitalRead(10);
@@ -279,18 +363,21 @@ void loop()
   }
   if(bullet[0]!=-1){
     bullet[0] +=1;
-    draw(Bullet,bullet[0],bullet[1]);
+    draw(Bullet,bullet[0],bullet[1],lD);
     for(int x = 0;x<obj_size;x++){
       if(Objects[x][2] == 0){
-        if(Objects[x][1] == bullet[1] && Objects[x][0] == bullet[0]){
+        if(Objects[x][1] == bullet[1] && Objects[x][0] <= bullet[0]){
           Objects[x][2] = 4;
+          del(Bullet,bullet[0],bullet[1],lD);
+          digitalWrite(41,HIGH);
           bullet[0]= -1;
           bullet[1]= -1;
           break;
          }
         }else if(Objects[x][2] == 4){
-          if(Objects[x][1] == bullet[1] && Objects[x][0] == bullet[0]){
+          if(Objects[x][1] == bullet[1] && Objects[x][0] <= bullet[0]){
             update_bar(x);
+            del(Bullet,bullet[0],bullet[1],lD);
             digitalWrite(41,HIGH);
             bullet[0]= -1;
             bullet[1]= -1;
@@ -300,42 +387,51 @@ void loop()
     }
   }
 
-  draw(Rocket,0,rocket_loc[1]);
+  draw(Rocket,0,rocket_loc[1],lD);
   display.display();
 
   int tmr = 0;
   if(tmr%20==0){
+    tmr+=1;
     display.display();
-    display.clearDisplay();
+    fill(lD);
+    if(bullet[0]!=-1)
+      draw(Bullet,bullet[0],bullet[1],lD);
     //move objects
     for(int x = 0;x<obj_size;x++){
       if(Objects[x][0] != -1){
-        Objects[x][0] = Objects[x][0]-1;
-        if(Objects[x][0] == -1){
-          update_bar(x);
-        }
+        Objects[x][0] -= 1;
       }
     }
 
-    draw(Rocket,0,rocket_loc[1]);
+    draw(Rocket,0,rocket_loc[1],lD);
 
     //control coll
     for(int x = 0;x<obj_size;x++){
       int ob = Objects[x][2];
       if(Objects[x][0] != -1){
         if(Objects[x][0] == rocket_loc[0] && Objects[x][1] == rocket_loc[1]){
-          if(ob == 0){
-            digitalWrite(41,HIGH);
-            delay(5000);
+          if(ob == 0 || ob == 4){
+            if(tch == 0){
+              // - crushed
+              life -= 1;
+              tch = 4000;
+              if(life<=0){//Dead
+                digitalWrite(41,HIGH);
+                delay(1000);
+                setup();
+                }
+              }
             }
             else if(ob == 1){
               
             }
             else if(ob == 2){
-              
+              if(gun <3)
+                gun += 1;
             }
             else if(ob == 3){
-              
+              tch = 4000;
             }
           }
       }
@@ -344,24 +440,24 @@ void loop()
       int ob = Objects[x][2];
       if(Objects[x][0] != -1){
         if(ob == 0){
-          draw(Rock,Objects[x][0],Objects[x][1]);
+          draw(Rock,Objects[x][0],Objects[x][1],lD);
         }
         else if(ob == 1){
-          draw(Prize,Objects[x][0],Objects[x][1]);
+          draw(Prize,Objects[x][0],Objects[x][1],lD);
         }
         else if(ob == 2){
-          draw(Gun,Objects[x][0],Objects[x][1]);
+          draw(Gun,Objects[x][0],Objects[x][1],lD);
         }
         else if(ob == 3){
-          draw(NTouch,Objects[x][0],Objects[x][1]);
+          draw(NTouch,Objects[x][0],Objects[x][1],lD);
         }else if(ob == 4){
-          draw(HRock,Objects[x][0],Objects[x][1]);
+          draw(HRock,Objects[x][0],Objects[x][1],lD);
         }
       }
     }
 
     //end of loop update gen objects of new frame
-    if(loop_c%2==0){
+    if(!random(0,5)){
       gen_met();
     }if(loop_c%14 == 0){
       //1 can 1 silah ekle
@@ -369,6 +465,14 @@ void loop()
         life += 1;
       if (gun <3)
         gun += 1;    
+    }
+
+    for(int x = 0;x<obj_size;x++){
+      if(Objects[x][0] != -1){
+        if(Objects[x][0] == 0){
+          update_bar(x);
+        }
+      }
     }
 
     // arayüz------------------------------
@@ -394,7 +498,7 @@ void loop()
         digitalWrite(22+i,LOW);
     }
     //  
-
+    tone(42,1000);
     // Gun
     digitalWrite(34,LOW);
     digitalWrite(35,LOW);
@@ -411,7 +515,22 @@ void loop()
       digitalWrite(35,HIGH);
       digitalWrite(36,HIGH);
     }
-    //
+    //Life
+    digitalWrite(37,LOW);
+    digitalWrite(38,LOW);
+    digitalWrite(39,LOW);
+    if(life == 1){
+      digitalWrite(34,HIGH);
+    }
+    else if(life == 2){
+      digitalWrite(37,HIGH);
+      digitalWrite(38,HIGH);
+    }
+    else if(life == 3){
+      digitalWrite(37,HIGH);
+      digitalWrite(38,HIGH);
+      digitalWrite(39,HIGH);
+    }    
 
     //zaman ayarlanması
     if(hardness){
@@ -419,9 +538,14 @@ void loop()
         time = time-(time/5);
       }
     }
-    loop_c += 1;
-    cnt += 1;
     display.display();
     delay(time);
   }
+  digitalWrite(40,LOW);
+  if(tch>=1000){
+    digitalWrite(40,HIGH);
+    tch -= 1000;
+  }
+  loop_c += 1;
+  cnt += 1;
 }
